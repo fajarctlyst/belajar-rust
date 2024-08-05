af34431698e16e06f08d4af050a976cf586ef442
Tim McNamara <code@timmcnamara.co.nz>
2024-08-05 13:43:12 +1200

    02_01b ready

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-use actix_web::{get, App, HttpServer};
-
-#[get("/healthz")]
-async fn liveness() -> &'static str {
-    "ok\r\n"
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let app = || App::new().service(liveness);
-
-    HttpServer::new(app).bind("127.0.0.1:8080")?.run().await
+fn main() {
+    println!("Hello, LinkedIn Learner!");
 }
