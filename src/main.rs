773946accd31ed93da6157a3ea901ce9735eb18f
Tim McNamara <code@timmcnamara.co.nz>
2024-09-24 03:43:06 +0000

    Create finished code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use serde::Serialize;
+use std::sync::Mutex;
 
 #[derive(Serialize)]
 struct Temperature {
@@ -7,8 +8,24 @@ struct Temperature {
     celsius: f32,
 }
 
+struct UsageStats {
+    to_fahrenheit: Mutex<u32>,
+    to_celsius: Mutex<u32>,
+}
+
+#[derive(Serialize)]
+struct UsageStatsResponse {
+    to_fahrenheit: u32,
+    to_celsius: u32,
+}
+
 #[get("/to-celsius/{fahrenheit}")]
-async fn to_celsius(f: web::Path<f32>) -> impl Responder {
+async fn to_celsius(f: web::Path<f32>, stats: web::Data<UsageStats>) -> impl Responder {
+    actix_web::rt::spawn(async move {
+        let mut count = stats.to_celsius.lock().unwrap();
+        *count += 1;
+    });
+
     let f = f.into_inner();
     let c = (f - 32.0) / 1.8;
     web::Json(Temperature {
@@ -18,7 +35,12 @@ async fn to_celsius(f: web::Path<f32>) -> impl Responder {
 }
 
 #[get("/to-fahrenheit/{celsius}")]
-async fn to_fahrenheit(c: web::Path<f32>) -> impl Responder {
+async fn to_fahrenheit(c: web::Path<f32>, stats: web::Data<UsageStats>) -> impl Responder {
+    actix_web::rt::spawn(async move {
+        let mut count = stats.to_fahrenheit.lock().unwrap();
+        *count += 1;
+    });
+
     let c = c.into_inner();
     let f = 32.0 + (c * 1.8);
     web::Json(Temperature {
@@ -27,10 +49,47 @@ async fn to_fahrenheit(c: web::Path<f32>) -> impl Responder {
     })
 }
 
+#[get("/usage-statistics")]
+async fn usage_statistics(stats: web::Data<UsageStats>) -> impl Responder {
+    let mut fahrenheit_count = stats.to_fahrenheit.lock().unwrap();
+    let mut celsius_count = stats.to_fahrenheit.lock().unwrap();
+
+    let response = UsageStatsResponse {
+        to_fahrenheit: *fahrenheit_count,
+        to_celsius: *celsius_count,
+    };
+
+    *fahrenheit_count = 0;
+    *celsius_count = 0;
+
+    web::Json(response)
+}
+
+#[post("/reset-usage-statistics")]
+async fn reset_usage_statistics(stats: web::Data<UsageStats>) -> impl Responder {
+    let mut fahrenheit_count = stats.to_fahrenheit.lock().unwrap();
+    let mut celsius_count = stats.to_fahrenheit.lock().unwrap();
+
+    *fahrenheit_count = 0;
+    *celsius_count = 0;
+
+    HttpResponse::NoContent()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(to_celsius).service(to_fahrenheit))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    let counts = web::Data::new(UsageStats {
+        to_fahrenheit: Mutex::new(0),
+        to_celsius: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(counts.clone())
+            .service(to_celsius)
+            .service(to_fahrenheit)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
